edae60b92d5b7d942181d360b279bcfecac7c2f2
Alkihis <alkihis@gmail.com>
2019-09-16 17:55:46 +0200

    New build

---
+++ deploy/static/www/precache-manifest.44148ce0a8af99fa8a04a988e9d90726.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ab3fb83c49fa2e2c67ffec791add32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d311536d298823c7560c",
+    "url": "/static/css/main.7fdb3e33.chunk.css"
+  },
+  {
+    "revision": "6f0742fe53aba265e151",
+    "url": "/static/js/2.fa7be377.chunk.js"
+  },
+  {
+    "revision": "d311536d298823c7560c",
+    "url": "/static/js/main.27105509.chunk.js"
+  },
+  {
+    "revision": "0111d660bb94abe69ef4",
+    "url": "/static/js/runtime~main.4100746b.js"
+  },
+  {
+    "revision": "67ca1e2def3d3a8af9287a5b00fd8c71",
+    "url": "/static/media/2.67ca1e2d.png"
+  },
+  {
+    "revision": "fe94000b3321e7dbadc2fb9bae07e15c",
+    "url": "/static/media/3.fe94000b.png"
+  },
+  {
+    "revision": "5d7814ab0f96e18aeb63be677779e83c",
+    "url": "/static/media/4.5d7814ab.png"
+  },
+  {
+    "revision": "bf7822c2ca13bcbd21bfcf017a1740b1",
+    "url": "/static/media/5.bf7822c2.png"
+  },
+  {
+    "revision": "87f885d3a62e2e375ad0488f478863cb",
+    "url": "/static/media/6.87f885d3.png"
+  },
+  {
+    "revision": "5bec24ec5725e8e6adcbb4daa3cdc977",
+    "url": "/static/media/7.5bec24ec.png"
+  },
+  {
+    "revision": "552d1322afba3bf2707ea751b8c4c50f",
+    "url": "/static/media/8.552d1322.png"
+  }
+]);
