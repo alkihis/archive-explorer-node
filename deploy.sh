6eb5b5d4d301470e741be9592908cb096c488e29
Alkihis <alkihis@gmail.com>
2019-09-22 13:53:54 +0200

    Better deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
+rm -rf ./static/www/*
+
+cp -R ../archive-explorer-web/build/* ./static/www
+
 mkdir deploy
 
 cp -R build misc .ssh static package.json settings.json deploy/
 
 zip -r deploy.zip deploy
+
+rm -rf deploy
